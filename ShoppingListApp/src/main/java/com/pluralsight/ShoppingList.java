0a2160dd8dd3273944443d54df347398e5a06a49
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-09 11:03:53 -0500

    completed ShoppingList app

---
+++ ShoppingListApp/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("My Shopping List:");
+        System.out.println("1. Milk");
+        System.out.println("2. Bread");
+        System.out.println("3. Eggs");
+        System.out.println("4. Rice");
+        System.out.println("5. Chicken");
+        System.out.println("6. Apples");
+        System.out.println("7. Oranges");
+        System.out.println("8. Toothpaste");
+        System.out.println("9. Shampoo");
+        System.out.println("10. Laundry detergent");
+    }
+}
