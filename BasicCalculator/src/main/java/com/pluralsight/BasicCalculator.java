7c5efce82407e3b32f5fca9ce4dec81a74ee211c
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-11 11:38:20 -0500

    Completed Mod3 - Exercise 3 - Basic Calculator

--- BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -13,16 +13,21 @@ public class BasicCalculator {
         System.out.print("Enter the second number : ");
         double number2 = scanner.nextDouble();
 
+        System.out.println();
+
         System.out.println("Possible calculations");
         System.out.println("(A)dd");
         System.out.println("(S)ubtract");
         System.out.println("(M)ultiply");
         System.out.println("(D)ivide");
 
+        System.out.println();
+
         System.out.println("Please select an option : ");
         String option = scanner.next();
 
         double result = number1 * number2;
+
 
         System.out.println( (int)number1 + " * " +  (int)number2 + " = " + (int)result);
 
